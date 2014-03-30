c81d0e333bec9302b8e574c17e46dfe583a5290f
Thomas <thombsaway@gmail.com>
2014-03-30 18:11:41 +1100

    Add comments to routes file

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,12 @@ exports.boxy = function(req, res){
   res.render('boxy', { title: 'boxy' });
 };
 
+// flappy_box page
 exports.flappy_box = function(req, res){
   res.render('flappy_box', {title: 'flappy_box'});
 };
 
+// everything else page, ie 404
 exports.none = function(req, res){
   res.render('index', { title: '404!'});
 };
