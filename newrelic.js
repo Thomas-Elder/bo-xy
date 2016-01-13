cb5a045fafc14a775e3e5c6b6d9a78bf863bacca
Thomas Elder <thomas.charles.elder@gmail.com>
2016-01-13 19:43:29 +1100

    Reverting...

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['boxy'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'edd21ff3a4925098fbe13ddc60334a724c164d49',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
--- public/dodge_box/box.js
+++ public/dodge_box/box.js
@@ -1,28 +1,28 @@
 // Defines the Box type.
-function PlayerBox(config, controller, context) {
+function Box(config, controller, context) {
   this.x            = (config.screenSize.width / 2) - (config.playerSize.width / 2);
   this.y            = config.screenSize.height - config.playerSize.height;
-  this.width        = config.box.player.size.width;
-  this.height       = config.box.player.size.height;
+  this.width        = config.playerSize.width;
+  this.height       = config.playerSize.height;
   this.screenWidth  = config.screenSize.width;
   this.screenHeight = config.screenSize.height;
-  this.speed        = config.box.player.speed;
+  this.speed        = config.playerSpeed;
   this.gravity      = config.gravity;
-  this.colour       = config.box.player.colour;
-  this.lives        = config.box.player.lives;
+  this.colour       = config.playerColour;
+  this.lives        = config.playerLives;
 
   this.controller   = controller;
   this.context      = context;
 }
 
 // Define the PlayerBox type's draw method.
-PlayerBox.prototype.draw = function() {
+Box.prototype.draw = function() {
   this.context.fillStyle = this.colour;
   this.context.fillRect(this.x, this.y, this.width, this.height);
 };
 
 // Define the PlayerBox type's update method.
-PlayerBox.prototype.update = function() {
+Box.prototype.update = function() {
   if (this.controller.left && this.x > 0) 
     this.x = this.x - this.speed;
 
@@ -41,11 +41,11 @@ PlayerBox.prototype.update = function() {
 };
 
 // Returns an object with x an y coords, the box's current position
-PlayerBox.prototype.getPosition = function() {
+Box.prototype.getPosition = function() {
   return {x: this.x, y: this.y};
 };
 
 // Returns an object with width and height of the box
-PlayerBox.prototype.getSize = function() {
+Box.prototype.getSize = function() {
   return {width: this.width, height: this.height};
 };
