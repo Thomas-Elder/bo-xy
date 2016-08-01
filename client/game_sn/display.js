9e7009300651c82e1828853385064563d2880a95
Thomas Elder <thomas.charles.elder@gmail.com>
2016-08-01 16:44:28 +1000

    Implemented test for interaction, more engine creation

--- client/game_sn/display.js
+++ client/game_sn/display.js
@@ -1,13 +1,94 @@
 
+
 /**
  * A class for handling calling any draw functions.
  */
-var Display = function(context){
-  this.context = context;
+var Display = function(config){
+  this.config = config;
 };
 
-Display.prototype.draw = function(){
+Display.prototype.init = function(){
 
+  // Get a reference to the canvas element.
+  var game_canvas = $('#game_canvas');
+
+  game_canvas.width   = config.screenSize.width;
+  game_canvas.height  = config.screenSize.height;
+
+  // Get a reference to the hud
+  var hud_canvas = $('#hud_canvas');
+
+  hud_canvas.width   = config.hudSize.width;
+  hud_canvas.height  = config.hudSize.height;
+
+  // Get the context.
+  this.game_context = game_canvas.getContext('2d');
+  this.hud_context  = hud_canvas.getContext('2d');
+};
+
+/**
+ * draw
+ * 
+ * @param state : an object describing the state of the game.
+ * The following fields are expected in the state object:
+ * background: a background object
+ * hud: a Hud object
+ * player: a PlayerBox object
+ * enemies: an array of EnemyBox objects
+ * explosions: an array of ExplodeBox objects
+ * powerboxes: an array of PowerBox objects
+ * 
+ */
+Display.prototype.draw = function(state){
+
+  // Clear the contexts for redraw
+  this.game_context.clearRect(0, 0, 
+                          this.config.screenSize.width, 
+                          this.config.screenSize.height);
+  this.hud_context.clearRect(0, 0, 
+                          this.config.hudSize.width, 
+                          this.config.hudSize.height);
+
+  // Draw the background
+  this.game_context.fillStyle = state.background.colour;
+  this.game_context.fillRect(0, 
+                          0, 
+                          this.config.screenSize.width, 
+                          this.config.screenSize.height);
+
+  // Draw the player box
+  this.game_context.fillStyle = state.player.colour;
+  this.game_context.fillRect(state.player.x, 
+                          state.player.y, 
+                          state.player.width, 
+                          state.player.height);
+
+  // Draw the enemy boxes
+  state.enemies.forEach(function(enemy){
+    this.game_context.fillStyle = enemy.colour;
+    this.game_context.fillRect(enemy.x, 
+                          enemy.y, 
+                          enemy.width, 
+                          enemy.height);
+  });
+
+  // Draw the explosions, if any
+  state.explosions.forEach(function(explosion){
+    this.game_context.fillStyle = explosion.colour;
+    this.game_context.fillRect(explosion.x, 
+                          explosion.y, 
+                          explosion.width, 
+                          explosion.height);
+  });
+
+  // Draw the powerboxes, if any
+  state.powerboxes.forEach(function(powerboxes){
+    this.game_context.fillStyle = powerboxes.colour;
+    this.game_context.fillRect(powerboxes.x, 
+                          powerboxes.y, 
+                          powerboxes.width, 
+                          powerboxes.height);
+  });
 };
 
 module.exports = Display;
--- client/game_sn/engine.js
+++ client/game_sn/engine.js
@@ -1,31 +1,77 @@
 
+// Box requirements
+var PlayerBox = require('./box/player_box');
+var EnemyBox = require('./box/enemy_box');
+var ExplodeBox = require('./box/explode_box');
+var PowerBox = require('./box/power_box');
+
+// Background and Hud
+var Background = require('./background');
+var Hud = require('./hud');
+
+// 
+var Controller = require('./controller');
+var Display = require('./display');
+var Interaction = require('./interaction');
+var Config = require('./config');
+
 /**
  * A class for managing the game.
  */
 var Engine = function(){
-
+  
 };
 
 Engine.prototype.init = function(){
 
+  this.config = new Config();
+  this.interaction = new Interaction();
+
+  this.display = new Display(this.config);
+
+  this.background = new Background(this.config);
+  this.hud = new Hud(this.config);
+
+  // Set up the controller to pass to the player
+  var controller   = new Controller();
+
+  window.onkeydown = function (event) { controller.keyDown(event); };
+  window.onkeyup   = function (event) { controller.keyUp(event); };
+
 };
 
 Engine.prototype.run() = function(){
+  
+  // loop
+  function loop(){
+    this.update();
+    this.draw();
 
-};
-
-Engine.prototype.collisionDetection = function(a, b) {
-  if ((a.getPosition().x - b.getSize().width) < b.getPosition().x) {
-    if (b.getPosition().x  < (a.getPosition().x + a.getSize().width)) {
-      if ((b.getPosition().y + b.getSize().height) > a.getPosition().y) {
-        if (b.getPosition().y < (a.getPosition().y + a.getSize().height)) {
-          return true;
-        }
-      }
-    }
+    // Check if game is over and clearInterval if so
+    
   }
 
-  return false;
+  var gameLoop = setInterval(loop, 1000 / this.config.fps);  
+};
+
+Engine.prototype.update = function(){
+
+  // update all
+  this.background.update();
+  this.hud.update();
+};
+
+Engine.prototype.draw() = function(){
+
+  // draw
+  this.display.draw({
+    background: this.background,
+    hud: this.hud,
+    player: this.player, 
+    enemies: this.enemies,
+    explosions: this.explosions,
+    powerboxes: this.powerboxes
+  });
 };
 
 Engine.prototype.newEnemyLocation = function(config) {
