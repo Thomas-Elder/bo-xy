2d31441b9679b312b0c22b9de583443f7d188d4e
Thomas <thombsaway@gmail.com>
2021-04-16 11:40:17 +1000

    Create star_box file and class

---
+++ client/game_sn/box/star_box.js
@@ -0,0 +1,40 @@
+
+// Defines the StarBox type.
+var StarBox = function (location, level, config) {
+  this.x            = location.x;
+  this.y            = location.y;
+  
+  this.width        = config.box.star.size.width;
+  this.height       = config.box.star.size.height;
+
+  this.screenWidth  = config.screenSize.width;
+  this.screenHeight = config.screenSize.height;
+
+  this.colour       = config.box.star.colour[level];
+  this.speed        = config.box.star.speed[level];
+
+  this.onScreen     = true;
+}
+
+// Define the StarBox type's update method.
+StarBox.prototype.update = function() {
+
+  // Move enemy down the screen
+  if (this.y + this.height < this.screenHeight + this.height)
+    this.y += this.speed;
+  else
+    this.onScreen = false;
+};
+
+// Returns an object with x an y coords, the box's current position
+StarBox.prototype.getPosition = function() {
+  return {x: this.x, y: this.y};
+};
+
+// Returns an object with width and height of the box
+StarBox.prototype.getSize = function() {
+  return {width: this.width, height: this.height};
+};
+
+
+module.exports = StarBox;
