70bb84b53bbb26cd26425d0990c87ead2f4b6731
Thomas <thombsaway@gmail.com>
2021-04-21 20:41:04 +1000

    Update engine so it uses background for the background now.

--- client/game_sn/engine.js
+++ client/game_sn/engine.js
@@ -42,6 +42,8 @@ Engine.prototype.init = function(contexts){
   this.display.init();
 
   this.background = new Background(config);
+  this.background.init();
+  
   this.hud = new Hud(config);
 
   // Set up the controller to pass to the player
@@ -52,6 +54,7 @@ Engine.prototype.init = function(contexts){
 
   this.boxManager = new BoxManager(config, controller);
   this.boxManager.init();
+
 };
 
 /**
@@ -81,6 +84,38 @@ Engine.prototype.run = function(){
  */
 Engine.prototype.update = function(){
 
+  // OOOOOOk I think what we want to do here is update some... 
+  // So conditionally update different parts as needed. 
+  // IE initially we just update background for 5 seconds?
+  // Then if a level changes we do that again. 
+  //
+  // Ok so what conditions do we have...
+  var intro = false;
+  var levelChange = false;
+  var outro = false;
+
+  if (intro) {
+    // update background
+    // update player
+    // after 5 seconds, update intro to false
+
+  } else if (levelChange) {
+    // update background
+    // update player
+    // after 5 seconds, update intro to false
+
+  } else if (outro) {
+    // update background
+    // after 5 seconds, endGame
+
+  } else {
+    // update background
+    // update player
+    // update enemies
+    // check for collisions
+    // if level changes, set levelChange = true
+  }
+
   // update all
   this.background.update();
   this.hud.update();
@@ -95,7 +130,7 @@ Engine.prototype.update = function(){
 };
 
 /**
- * 
+ * draw
  */
 Engine.prototype.draw = function(){
 
@@ -106,8 +141,8 @@ Engine.prototype.draw = function(){
     player: this.boxManager.playerBox, 
     enemies: this.boxManager.enemyBoxes,
     explosions: this.boxManager.explodeBoxes,
-    farStars: this.boxManager.farStarBoxes,
-    nearStars: this.boxManager.nearStarBoxes,
+    farStars: this.background.farStarBoxes,
+    nearStars: this.background.nearStarBoxes,
     powerboxes: []
   });
 
