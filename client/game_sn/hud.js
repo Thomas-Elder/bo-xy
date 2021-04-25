dcfae73763cb733292b6ec78b9472c9cf9e0c07a
Thomas <thombsaway@gmail.com>
2021-04-25 15:12:36 +1000

    Update hud to be a class

--- client/game_sn/hud.js
+++ client/game_sn/hud.js
@@ -1,31 +1,31 @@
 
-Hud = function(config, context) {
+export class hud {
+
+  constructor(config, context) {
   this.config  = config;
   this.context = context;
 
   this.score   = 0;
   this.level   = 0;
   this.lives   = config.playerLives;
+  }
+
+  draw() {
+    this.context.font = '20px sans-serif';
+
+    var scoreString = 'score: ' + this.score;
+    var levelString = 'level: ' + this.level;
+    var livesString = 'lives: ' + this.lives;
+
+    this.context.fillStyle = '#3399FF';
+    this.context.fillText(scoreString, 10, 20);
+    this.context.fillText(levelString, 130, 20);
+    this.context.fillText(livesString, 250, 20);
+  }
+
+  update(score, level, lives) {
+    this.score = score;
+    this.level = level;
+    this.lives = lives;
+  }
 }
-
-Hud.prototype.draw = function() {
-  this.context.font = '20px sans-serif';
-
-  var scoreString = 'score: ' + this.score;
-  var levelString = 'level: ' + this.level;
-  var livesString = 'lives: ' + this.lives;
-
-  this.context.fillStyle = '#3399FF';
-  this.context.fillText(scoreString, 10, 20);
-  this.context.fillText(levelString, 130, 20);
-  this.context.fillText(livesString, 250, 20);
-};
-
-Hud.prototype.update = function(score, level, lives) {
-  this.score = score;
-  this.level = level;
-  this.lives = lives;
-};
-
-
-module.exports = Hud;
