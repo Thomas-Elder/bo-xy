6009d17514517dcefe10ade766245af911d031eb
Thomas <thombsaway@gmail.com>
2021-04-25 15:09:26 +1000

    Remove unused imports from boxmanager

--- client/game_sn/box/box_manager.js
+++ client/game_sn/box/box_manager.js
@@ -2,11 +2,10 @@
 var PlayerBox = require('./player_box.js');
 var EnemyBox = require('./enemy_box.js');
 var ExplodeBox = require('./explode_box.js');
-var StarBox = require('./star_box.js');
 
 var Interaction = require('../interaction');
 
-const {Howl, Howler} = require('howler');
+const {Howl} = require('howler');
 var explosion = new Howl({src:['static/game_sn/media/explosion.mp3'], volume: 0.5});
 
 /**
