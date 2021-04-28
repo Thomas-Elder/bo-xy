e65f27dbcedfade320406a1b81a0df1e769f39ae
Thomas <thombsaway@gmail.com>
2021-04-28 14:00:58 +1000

    Reformatted spec, with auto formatter, then to move registering event handlers to the top of the it() block.

--- spec/server/events_single_spec.js
+++ spec/server/events_single_spec.js
@@ -3,80 +3,77 @@ var Server = require('../../server/server');
 var request = require('request');
 var io_client = require('socket.io-client');
 
-describe('Single events', 
-  function(){
+describe('Single events',
+  function () {
 
     // Declared here to be accessible in all functions. 
     var server;
     var url = 'http://localhost:8888/mingle';
     var socketOptions = {
-          'reconnection delay' : 0, 
-          'reopen delay' : 0, 
-          'force new connection' : true
-        };
+      'reconnection delay': 0,
+      'reopen delay': 0,
+      'force new connection': true
+    };
 
-    var client_emit;
-    var client_rcv;
+    var client;
 
     // Spin up server before all tests
-    beforeAll(function(){
+    beforeAll(function () {
 
       server = new Server();
-      console.log('Starting the server...');
       server.start();
     });
-    
+
     // Close server after all tests
-    afterAll(function(){
+    afterAll(function () {
       server.stop();
     });
 
     beforeEach(
-      function(done){
+      function (done) {
 
         // Connect a client socket to the server
         client = io_client(url, socketOptions);
 
         // Log connection
         client.on('ack',
-          function(){
-            console.log('socket_emit connected.');
+          function () {
             done();
-        });
+          });
 
         // Log connection error
         client.on('connect_error',
-          function(){
+          function () {
             console.log('socket_emit not connected, there was an error.');
             done();
-        });
-    });
-    
+          });
+      });
+
     afterEach(
-      function(done){
+      function (done) {
 
         client.disconnect(true);
         done();
-    });
+      });
 
-    describe('ack', function(){
+    describe('ack', function () {
 
-      it('should emit "ack" event to this client on connection', function(done){           
-        client.on('ack', function(msg){
+      it('should emit "ack" event to this client on connection', function (done) {
+        client.on('ack', function (msg) {
           expect(true).toEqual(true);
           done();
         });
 
         client.disconnect();
-        client.connect(url, socketOptions); 
+        client.connect(url, socketOptions);
       });
     });
 
-    describe('score', function(){
-      
-      it('should', function(done){
+    describe('score', function () {
+
+      it('should', function (done) {
         done();
       });
 
     });
-});
+  });
