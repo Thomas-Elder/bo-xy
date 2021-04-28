88d43382af4ca49656b8d90ec7f7c21c4902f573
Thomas <thombsaway@gmail.com>
2021-04-28 13:59:55 +1000

    Reformat spec, using auto format first, then moving assigning callbacks to the beginning of the it() call, so they're registered before the event is fired.

--- spec/server/events_mingle_spec.js
+++ spec/server/events_mingle_spec.js
@@ -5,35 +5,34 @@ var Server = require('../../server/server');
 var request = require('request');
 var io_client = require('socket.io-client');
 
-describe('Lobby events', 
-  function(){
+describe('Lobby events',
+  function () {
 
     // Declared here to be accessible in all functions. 
     var server;
     var url = 'http://localhost:8888/mingle';
     var socketOptions = {
-          'reconnection delay' : 0, 
-          'reopen delay' : 0, 
-          'force new connection' : true
-        };
+      'reconnection delay': 0,
+      'reopen delay': 0,
+      'force new connection': true
+    };
 
     var client_emit;
     var client_rcv;
 
     // Spin up server before all tests
-    beforeAll(function(){
+    beforeAll(function () {
 
       server = new Server();
-      console.log('Starting the server...');
       server.start();
     });
-    
+
     // Close server after all tests
-    afterAll(function(){
+    afterAll(function () {
       server.stop();
     });
 
-    beforeEach(function(done){
+    beforeEach(function (done) {
 
       // Connect a client socket to the server
       client_emit = io_client(url, socketOptions);
@@ -42,18 +41,18 @@ describe('Lobby events',
       client_rcv = io_client(url, socketOptions);
 
       // Log connection
-      client_emit.on('ack', function(){
+      client_emit.on('ack', function () {
         //console.log('socket_emit connected.');
       });
 
-      client_rcv.on('ack', function(){
+      client_rcv.on('ack', function () {
         //console.log('client_rcv connected.');
         done();
       });
     });
-    
-    afterEach(function(done){
-        
+
+    afterEach(function (done) {
+
       // Disconnect both sockets
       client_emit.disconnect(0);
       client_rcv.disconnect(0);
@@ -61,66 +60,65 @@ describe('Lobby events',
       done();
     });
 
-    describe('connection', function(){
-      console.log('');
-      console.log('testing connection... ');
-        
-      it('should emit "connect" event to this client on connection', function(done){
+    describe('connection', function () {
 
-        client_emit.on('ack', function(msg){
+      it('should emit "connect" event to this client on connection', function (done) {
+
+        client_emit.on('ack', function (msg) {
           expect(true).toEqual(true);
           done();
         });
 
         client_emit.disconnect();
-        client_emit.connect(url, socketOptions);               
+        client_emit.connect(url, socketOptions);
       });
 
-      it('should emit "newPlayer" event to other clients in the namespace', function(done){
-        client_rcv.on('newPlayer', function(){
+      it('should emit "newPlayer" event to other clients in the namespace', function (done) {
+        client_rcv.on('newPlayer', function () {
           expect(true).toEqual(true);
           done();
         });
 
         client_emit.disconnect();
-        client_emit.connect(url, socketOptions); 
+        client_emit.connect(url, socketOptions);
       });
     });
-    
-    describe('open', function(){
-      console.log('');
-      console.log('testing open... ');
 
-      it('should emit "newLobby" event to other clients, on "open" event', function(done){
+    describe('open', function () {
 
-        client_emit.emit('open', {msg:"open message"});
-
-        client_rcv.on('newLobby', function(lobby) {
+      it('should emit "newLobby" event to other clients, on "open" event', function (done) {
+        
+        client_rcv.on('newLobby', function (lobby) {
           expect(true).toEqual(true);
-          done(); 
-        }); 
+          done();
+        });
+
+        client_emit.emit('open', { msg: "open message" });
       });
 
-      it('should pass all lobby details on in the "newLobby" event', function(done){
+      it('should pass all lobby details on in the "newLobby" event', function (done) {
+
+        client_emit.on('newLobby', function (lobby) {
+          expect(lobby).toEqual(expected);
+          done();
+        });
 
         var socket_id = client_emit.id;
-        var expected = {users:[socket_id], id:socket_id};
+        var expected = { users: [socket_id], id: socket_id };
 
-        client_emit.emit('open');
-
-        client_emit.on('newLobby', function(lobby) {
-          expect(lobby).toEqual(expected);
-          done(); 
-        });
+        client_emit.emit('open');   
       });
     });
 
-    describe('join', function(){
-      console.log('');
-      console.log('testing join... ');
+    describe('join', function () {
 
-      it('should emit a "PlayerJoined" event when a join event is handled', function(done){
-        
+      it('should emit a "PlayerJoined" event when a join event is handled', function (done) {
+
+        client_emit.on('PlayerJoined', function (lobby) {
+          expect(true).toEqual(true);
+          done();
+        });
+
         // First get the socket.id from client_emit, to host the lobby
         var lobby_id = client_emit.id;
 
@@ -128,42 +126,18 @@ describe('Lobby events',
         client_emit.emit('open', {});
 
         // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
+        var lobby = { users: [lobby_id], id: lobby_id };
 
-        client_rcv.emit('join', lobby);
-
-        client_emit.on('PlayerJoined', function(lobby) {
-          expect(true).toEqual(true);
-          done(); 
-        });
+        client_rcv.emit('join', lobby);        
       });
 
-      it('should pass the lobby details to the original client', function(done){
-        
-        // First get the socket.id from client_emit, to host the lobby
-        var lobby_id = client_emit.id;
+      it('should pass the lobby details to the original client', function (done) {
 
-        // Open a lobby
-        client_emit.emit('open', {});
-
-        // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
-
-        // Get the client_rcv socket.id to join the lobby with
-        var join_id = client_rcv.id
-        
-        var expected = {users:[lobby_id, join_id], id:lobby_id};
-
-        client_rcv.emit('join', lobby);
-
-        client_emit.on('PlayerJoined', function(lobby) {
+        client_emit.on('PlayerJoined', function (lobby) {
           expect(lobby.users[0]).toEqual(expected.users[0]);
           expect(lobby.id).toEqual(expected.id);
-          done(); 
+          done();
         });
-      });
-
-      it('should pass the lobby details to the new client', function(done){
 
         // First get the socket.id from client_emit, to host the lobby
         var lobby_id = client_emit.id;
@@ -172,30 +146,50 @@ describe('Lobby events',
         client_emit.emit('open', {});
 
         // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
+        var lobby = { users: [lobby_id], id: lobby_id };
 
         // Get the client_rcv socket.id to join the lobby with
         var join_id = client_rcv.id
-        
-        var expected = {users:[lobby_id, join_id], id:lobby_id};
 
-        console.log('Logging lobby object in spec, ', lobby)
+        var expected = { users: [lobby_id, join_id], id: lobby_id };
+
         client_rcv.emit('join', lobby);
+      });
 
-        client_rcv.on('PlayerJoined', function(lobby) {
+      it('should pass the lobby details to the new client', function (done) {
+
+        client_rcv.on('PlayerJoined', function (lobby) {
           expect(lobby.users[0]).toEqual(expected.users[0]);
           expect(lobby.id).toEqual(expected.id);
-          done(); 
+          done();
         });
+
+        // First get the socket.id from client_emit, to host the lobby
+        var lobby_id = client_emit.id;
+
+        // Open a lobby
+        client_emit.emit('open', {});
+
+        // Create lobby object 
+        var lobby = { users: [lobby_id], id: lobby_id };
+
+        // Get the client_rcv socket.id to join the lobby with
+        var join_id = client_rcv.id
+
+        var expected = { users: [lobby_id, join_id], id: lobby_id };
+
+        client_rcv.emit('join', lobby);
       });
     });
 
-    describe('bail', function(){
-      console.log('');
-      console.log('testing bail... ');
+    describe('bail', function () {
+      it('should emit a "bailLobby" event to this client when the bail event is handled', function () {
 
-      it('should emit a "bailLobby" event to this client when the bail event is handled', function(){
-        
+        client_emit.on('bailLobby', function () {
+          expect(true).toEqual(true);
+          done();
+        });
+
         // First get the socket.id from client_emit, to host the lobby
         var lobby_id = client_emit.id;
 
@@ -203,7 +197,7 @@ describe('Lobby events',
         client_emit.emit('open', {});
 
         // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
+        var lobby = { users: [lobby_id], id: lobby_id };
 
         // Get the client_rcv socket.id to join the lobby with
         var join_id = client_rcv.id
@@ -211,16 +205,16 @@ describe('Lobby events',
         // join the lobby
         client_rcv.emit('join', lobby);
 
-        client_emit.emit('bail', lobby);
-
-        client_emit.on('bailLobby', function(){
-          expect(true).toEqual(true);
-          done(); 
-        });
+        client_emit.emit('bail', lobby);        
       });
 
-      it('should emit a "bailLobby" event to other clients when the bail event is handled', function(){
+      it('should emit a "bailLobby" event to other clients when the bail event is handled', function () {
         
+        client_rcv.on('bailLobby', function () {
+          expect(true).toEqual(true);
+          done();
+        });
+
         // First get the socket.id from client_emit, to host the lobby
         var lobby_id = client_emit.id;
 
@@ -228,7 +222,7 @@ describe('Lobby events',
         client_emit.emit('open', {});
 
         // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
+        var lobby = { users: [lobby_id], id: lobby_id };
 
         // Get the client_rcv socket.id to join the lobby with
         var join_id = client_rcv.id
@@ -236,48 +230,19 @@ describe('Lobby events',
         // join the lobby
         client_rcv.emit('join', lobby);
 
-        client_emit.emit('bail', lobby);
-
-        client_rcv.on('bailLobby', function(){
-          expect(true).toEqual(true);
-          done(); 
-        });
+        client_emit.emit('bail', lobby);        
       });
     });
-/**
-    describe('start', function(){
-      console.log('');
-      console.log('testing start... ');
 
-      it('should emit a "start" event to this client when the start event is handled', function(){
+    describe('start', function () {
+
+      it('should emit a "start" event to this client when the start event is handled', function () {
         
-        // First get the socket.id from client_emit, to host the lobby
-        var lobby_id = client_emit.id;
-
-        // Open a lobby
-        client_emit.emit('open', {});
-
-        // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
-
-        // Get the client_rcv socket.id to join the lobby with
-        var join_id = client_rcv.id
-
-        console.log('Logging lobby object in spec, ', lobby)
-
-        // join the lobby
-        client_rcv.emit('join', lobby);
-
-        client_emit.emit('start', lobby);
-
-        client_emit.on('start', function(){
+        client_emit.on('start', function () {
           expect(true).toEqual(true);
-          done(); 
+          done();
         });
-      });
 
-      it('should emit a "start" event to other clients when the start event is handled', function(){
-        
         // First get the socket.id from client_emit, to host the lobby
         var lobby_id = client_emit.id;
 
@@ -285,7 +250,7 @@ describe('Lobby events',
         client_emit.emit('open', {});
 
         // Create lobby object 
-        var lobby = {users: [lobby_id], id: lobby_id};
+        var lobby = { users: [lobby_id], id: lobby_id };
 
         // Get the client_rcv socket.id to join the lobby with
         var join_id = client_rcv.id
@@ -294,11 +259,30 @@ describe('Lobby events',
         client_rcv.emit('join', lobby);
 
         client_emit.emit('start', lobby);
-
-        client_rcv.on('start', function(){
-          expect(true).toEqual(true);
-          done(); 
-        });
       });
-    }); */
-});
+
+      it('should emit a "start" event to other clients when the start event is handled', function () {
+
+        // Set up listener for event
+        client_rcv.on('start', function () {
+          expect(true).toEqual(true);
+          done();
+        });
+
+        // Get the socket.id from client_emit, to host the lobby
+        var lobby_id = client_emit.id;
+
+        // Open a lobby
+        client_emit.emit('open', {});
+
+        // Create lobby object 
+        var lobby = { users: [lobby_id], id: lobby_id };
+
+        // join the lobby
+        client_rcv.emit('join', lobby);
+
+        // emit the start event
+        client_emit.emit('start', lobby);
+      });
+    });
+  });
