0ace218e289964dd981c9e2d07ba984f2c22ab92
Thomas <thombsaway@gmail.com>
2021-04-29 20:14:33 +1000

    Add tests for boxManager constructor and stub out further tests.

--- spec/client/game_sn/box/boxManager.spec.mjs
+++ spec/client/game_sn/box/boxManager.spec.mjs
@@ -1,58 +1,116 @@
 
-// const config = require('../../../client/game_sn/config.js');
-//const PlayerBox = require('../../../../client/game_sn/box/boxes.js');
-//const EnemyBox = require('../../../../client/game_sn/box/boxes.js');
-//const ExplodeBox = require('../../../../client/game_sn/box/boxes.js');
-//const Starbox = require('../../../../client/game_sn/box/boxes.js');
+import { config } from '../../../../client/game_sn/config.mjs';
+import { StarBox, PlayerBox, EnemyBox, ExplodeBox } from '../../../../client/game_sn/box/boxes.mjs';
+import { BoxManager } from '../../../../client/game_sn/box/box_manager.mjs'
+import { Controller } from '../../../../client/game_sn/controller.mjs';
 
-import {StarBox, PlayerBox, EnemyBox, ExplodeBox} from '../../../../client/game_sn/box/boxes.mjs';
-import {BoxManager} from '../../../../client/game_sn/box/box_manager.mjs'
-
-//import pkg from '../../../../client/game_sn/box/boxes.mjs';
-//const {StarBox, PlayerBox, EnemyBox, ExplodeBox} = pkg;
+var controller;
+var boxManager;
 
 describe('Box Manager',
-  function(){
+  function () {
 
     beforeEach(
-      function(done){
+      function (done) {
+        controller = new Controller();
+        boxManager = new BoxManager(config, controller);
         done();
-    });
-    
+      });
+
     afterEach(
-      function(done){
-        done();
-    }); 
-
-    describe('init', function(){
-      it('should create a new Interaction instance', function(done){
-
+      function (done) {
+        controller = null;
+        boxManager = null;
         done();
       });
 
-      it('should create a new PlayerBox instance', function(done){
-
+    describe('constructor', function () {
+      it('should create a new Interaction instance', function (done) {
+        expect(boxManager.interaction).not.toBe(null);
         done();
       });
 
-      it('should create an array of EnemyBox instances', function(done){
+      it('should create a new PlayerBox instance', function (done) {
+        expect(boxManager.playerBox).not.toBe(null);
+        done();
+      });
+
+      it('should create an array for EnemyBoxes', function (done) {
+        expect(boxManager.enemyBoxes).not.toBe(null);
+        done();
+      });
+
+      it('should create an array for EnemyBoxes of config length', function (done) {
+        expect(boxManager.enemyBoxes.length).toBe(config.numberOfEnemies);
+        done();
+      });
+
+      it('should create an array for nearStarBoxes', function (done) {
+        expect(boxManager.nearStarBoxes).not.toBe(null);
+        done();
+      });
+
+      it('should create an array for nearStarBox of config length', function (done) {
+        expect(boxManager.nearStarBoxes.length).toBe(config.numberOfNearStars);
+        done();
+      });
+
+      it('should create an array for farStarBoxes', function (done) {
+        expect(boxManager.farStarBoxes).not.toBe(null);
+        done();
+      });
+
+      it('should create an array for farStarBox of config length', function (done) {
+        expect(boxManager.farStarBoxes.length).toBe(config.numberOfFarStars);
+        done();
+      });
+    });
+
+    describe('updatePlayer', function () {
+      it('should call the player.update function', function (done) {
 
         done();
       });
     });
 
-    describe('update', function(){
-      it('should call the player.update function', function(done){
+    describe('updateEnemies', function () {
+      it('should call the enemy.update function', function (done) {
 
         done();
       });
     });
 
-    describe('enemyHit', function(){
-      it('', function(){});
+    describe('updateBackground', function () {
+      it('should call the star.update function', function (done) {
+
+        done();
+      });
     });
 
-    describe('newEnemyLocation', function(){
-      it('', function(){});
+    describe('updateExplosions', function () {
+      it('should call the explode.update function', function (done) {
+
+        done();
+      });
     });
-});
+
+    describe('clearEnemies', function () {
+      it('', function () { });
+    });
+
+    describe('enemyHit', function () {
+      it('', function () { });
+    });
+
+    describe('newEnemyLocation', function () {
+      it('', function () { });
+    });
+
+    describe('startStarLocation', function () {
+      it('', function () { });
+    });
+
+    describe('newStarLocation', function () {
+      it('', function () { });
+    });
+  });
