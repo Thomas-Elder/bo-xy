f64f75a6a3b6b7a2f8ad707b775c2356ad64c801
Thomas <thombsaway@gmail.com>
2021-05-07 16:23:05 +1000

    Update engine to handle controller init. Removed more hud calls.

--- client/game_sn/engine.js
+++ client/game_sn/engine.js
@@ -4,7 +4,11 @@ import {Hud} from './hud';
 import {Display} from './display';
 import {Controller} from './controller.mjs';
 import {config} from './config';
-import {EnemyBox, PlayerBox} from './box/boxes.mjs';
+import {EnemyBox, PlayerBox, ExplodeBox} from './box/boxes.mjs';
+import {Utility} from './utility.mjs'
+
+import { Howl } from 'howler';
+var explosion = new Howl({ src: ['static/game_sn/media/explosion.mp3'], volume: 0.3 });
 
 /**
  * A class for managing the game.
@@ -25,28 +29,33 @@ export class Engine {
     this.level_score  = 0;
     this.level        = 0;
     this.lives        = config.box.player.lives;
+    this.enemiesHit = 0;
 
     this.max_level    = config.numberOfLevels;
     this.config = config;
 
     this.display = new Display(config, contexts);
-    
-    this.hud = new Hud(this.config);
 
     // Set up the controller to pass to the player
-    var controller   = new Controller();
+    this.controller   = new Controller();
 
     // Register the controllers event listeners
-    window.onkeydown = function (event) { controller.keyDown(event); };
-    window.onkeyup   = function (event) { controller.keyUp(event); };
+    window.onkeydown = function (event) { self.controller.keyDown(event); };
+    window.onkeyup   = function (event) { self.controller.keyUp(event); };
 
-    this.boxManager = new BoxManager(this.config, controller);
+    this.boxManager = new BoxManager(this.config, this.controller);
 
-    this.player = new PlayerBox(this.config, controller);
+    // Instantiate the player
+    this.player = new PlayerBox(this.config, this.controller);
+
+    // Instantiate the enemies
     this.enemies = new Array(this.config.numberOfEnemies);
 
     for (var i = 0; i < this.config.numberOfEnemies; i++)
       this.enemies[i] = new EnemyBox(1, this.config);
+  
+    // Instantiate the explode boxes array.
+    self.explosions = [];
   }
 
   /**
@@ -70,7 +79,7 @@ export class Engine {
         hud: self.hud,
         player: self.player, 
         enemies: self.enemies,
-        explosions: self.boxManager.explodeBoxes,
+        explosions: self.explosions,
         farStars: self.boxManager.farStarBoxes,
         nearStars: self.boxManager.nearStarBoxes,
         powerboxes: []
@@ -80,7 +89,6 @@ export class Engine {
       if (introCount != self.config.introDuration) {
         // Update everything we want on the screen during this
         self.boxManager.updateBackground();
-        self.hud.update();
         self.player.update();
 
         // Then draaw it all
@@ -99,9 +107,8 @@ export class Engine {
       } else if (levelChangeCount != self.config.levelChangeDuration) {
         // Update everything we want on the screen during this
         self.boxManager.updateBackground();
-        self.hud.update();
         self.player.update();
-        self.boxManager.updateExplosions();
+        self.updateExplosions();
 
         // Then draaw it all
         self.display.drawClear();
@@ -119,8 +126,7 @@ export class Engine {
         // Then we need to do the same at the end... 
       } else if (outroCount != self.config.outroDuration) {
         self.boxManager.updateBackground();
-        self.hud.update();
-        self.boxManager.updateExplosions();
+        self.updateExplosions();
 
         self.display.drawClear();
         self.display.drawBackground(state);
@@ -138,12 +144,11 @@ export class Engine {
 
         // Update the game  
         self.boxManager.updateBackground();
-        self.hud.update();
         self.player.update();
-        self.boxManager.updateExplosions();
+        self.updateExplosions();
         self.enemies.forEach((enemy) => {enemy.update()});
-        self.boxManager.enemyHit();
-        self.lives = config.box.player.lives - self.boxManager.enemiesHit;
+        self.collision();
+        self.lives = self.player.lives;
         self.score = self.boxManager.enemiesDodged;
 
         // Check if the level changed
@@ -153,7 +158,7 @@ export class Engine {
         if (self.level != currentLevel) {
           levelChangeCount = 0;
           self.level = currentLevel;
-          self.boxManager.clearEnemies(self.level);
+          //self.boxManager.clearEnemies(self.level);
         }
 
         // And the draws...
@@ -241,5 +246,53 @@ export class Engine {
 
     // Emit event with game details object to add to highscores
     this.socket.emit('score', gameDetails);
+  }
+
+  collision(){
+    var self = this;
+
+    // are we getting here
+    console.log('in collision... ');
+
+    if (!self.player.isBlinking) {
+      
+      self.enemies.forEach(function (enemy) {
+        if (!enemy.hit) {
+          console.log('enemy is on screen... ')
+          if (Utility.overlap(self.player, enemy)) {
+            explosion.play();
+            enemy.hit = true;
+            self.explosions.push(new ExplodeBox(enemy.x, enemy.y,
+              self.config));
+
+            self.player.lives--;
+            self.enemiesHit++;
+
+            self.player = new PlayerBox(self.config, self.controller);
+          }
+        }
+      });
+    }
+  }
+
+    /**
+   * updateExplosions
+   * Iterates through the explode box array, calling update until
+   * endOfExplode returns true.
+   */
+  updateExplosions() {
+
+    var self = this;
+
+    if (self.explosions.length > 0) {
+      for (var k = 0; k < self.explosions.length; k++) {
+
+        self.explosions[k].update();
+
+        if (self.explosions[k].endOfExplode()) {
+          self.explosions.splice(k, 1);
+        }
+      }
+    }
   }
 }
